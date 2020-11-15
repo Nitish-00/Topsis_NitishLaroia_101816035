49f2d2b71b3c654fb600c3c775f7ab7f79a56603
Nitish-00 <74503029+Nitish-00@users.noreply.github.com>
2020-11-15 21:54:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'This library helps in implementing topsis method',   # Give a short description about your library
   author = 'Nitish Laroia',                   # Type in your name
   author_email = 'nlaroia_be18@thapar.edu',      # Type in your E-Mail
-  url = 'https://github.com/Kanav-119/Topsis_KanavMehta_101816020',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Kanav-119/Topsis_KanavMehta_101816020/archive/v1.0.tar.gz',    # I explain this later on
+  url = 'https://github.com/Nitish-00/Topsis_NitishLaroia_101816035',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Nitish-00/Topsis_NitishLaroia_101816035/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'TOPSIS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'os',
